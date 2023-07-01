4526cbce4dad697a1442db345b0981ef3db7bb4a
43z708 <s7080303r@gmail.com>
2023-07-01 17:38:55 +0900

    Remove unnecessary youtube file

--- main.go
+++ main.go
@@ -2,14 +2,6 @@ package main
 
 import (
 	"app/infrastructure"
-	"context"
-
-	"google.golang.org/api/youtube/v3"
-)
-
-var (
-	ctx   context.Context
-	ytSvc *youtube.Service
 )
 
 func main() {
@@ -18,16 +10,4 @@ func main() {
 	infrastructure.Router(Init)
 	infrastructure.Discord(Init)
 
-	// infrastructure.Youtube()
-
-	// // Discordセッションの作成
-	// controller.CreateSession()
-	// // Discordセッションを開始
-	// controller.StartSession()
-	// // YouTubeクライアントの作成
-	// controller.CreateClient()
-	// // cronの実行
-	// controller.StartCron()
-
-	// log.Println("Bot stopped.")
 }
